868e1acc11e6428711d085d021c2317163172a7a
Stephen Aponte <saponte1987@gmail.com>
2018-10-01 20:57:53 -0600

    First commit

---
+++ src/components/app.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className='app'>
+        <h1>Blade</h1>
+        <h2>Venom</h2>
+        <h2>Batman</h2>        
+      </div>
+    );
+  }
+}
